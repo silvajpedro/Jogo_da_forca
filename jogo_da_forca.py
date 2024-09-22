ccdf5a6648fa616cffb4ee3b885568f71b7cab68
silvajpedro <belojpedro.31@gmail.com>
2024-09-22 11:58:33 -0300

    :broom: cleanup: Removing and Adjusting conditional structures

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,6 +1,6 @@
 import time
 
-palavra = "JavaScript"
+palavra = "REACT"
 
 jubileu = [
  "o",
@@ -24,9 +24,6 @@ jubileu_ganhou = [
  " |",      
 "/ \\"     ]
 
-
-placar = 6
-
 def iniciar_jogo():
     
     palavra_correta = (len(palavra) * "-")
@@ -37,15 +34,14 @@ def iniciar_jogo():
     time.sleep(2)
     print("\nRegras do jogo:\n")
     print("- O Jubileu está em perigo! A cada erro, uma parte do corpo dele será enforcada.")
-    print(f"- Você tem {len(palavra)+2} tentativas para adivinhar a palavra correta e salvar o Jubileu!")
+    print(f"- Você tem {len(palavra)+1} tentativas para adivinhar a palavra correta e salvar o Jubileu!")
     print("- Se errar todas as vezes, Jubileu será enforcado! 😢")
-    print("- Cada letra errada desenha uma parte do Jubileu na forca. Boa sorte!\n")
 
     print(f'   {jubileu[0]} \n {jubileu[1]} {jubileu[2]} {jubileu[3]} \n   {jubileu[4]} \n  {jubileu[5]} {jubileu[6]}')
     
     time.sleep(2)
     
-    count = len(palavra)+2
+    count = len(palavra)+1
 
     while count >= 1:
 
@@ -55,15 +51,9 @@ def iniciar_jogo():
 
         print(f'\n {palavra_correta} ')
 
-        resposta = input("\nDigite alguma letra da palavra: ")
+        resposta = input("\nDigite alguma letra da palavra: ").upper()
 
-        if "".join(lista_palavra_correta) == palavra:
-            print("\nMeus parabéns, você conseguiu vencer o jogo da forca!!!")
-            print("Jubileu deve à vida a você, veja sua comemoração!!!")
-            print(f' {jubileu_ganhou[0]} \n {jubileu_ganhou[1]} \n {jubileu_ganhou[2]} \n {jubileu_ganhou[3]} \n ')
-            break
-
-        elif resposta in palavra:
+        if resposta in palavra:
             
             print(f"\nMuito bem, a letra {resposta} existe nessa palavra!!!\n")
 
@@ -74,8 +64,13 @@ def iniciar_jogo():
                         
             palavra_correta = "".join(lista_palavra_correta)
 
-        elif resposta in palavra and palavra_correta == palavra:
-            print(f'Parabéns, você ZEROU A FORCA E SALVOU JUBILEU DE SEU DESTINO CRUEL')
+            if "".join(lista_palavra_correta) == palavra:
+                print("\nMeus parabéns, você conseguiu vencer o jogo da forca!!!")
+                print(f"\nA palavra era {palavra}\n")
+                print("Jubileu deve à vida a você, veja sua comemoração!!!")
+                print(f' {jubileu_ganhou[0]} \n {jubileu_ganhou[1]} \n {jubileu_ganhou[2]} \n {jubileu_ganhou[3]} \n ')
+                break
+
         else:
             print(f'\n \nNegativo!!! a letra {resposta} não existe nessa palavra, Jubileu está mais perto do seu fim!!! >:D ')
             count-=1
@@ -85,7 +80,7 @@ def iniciar_jogo():
 
         if count == 0:
             print(f'Hehehe a forca venceu como era previsto, era uma vez Jubileu...')
-            print(f'\n {jubileu_perdeu} \n')
+            print(f'\n {jubileu_perdeu[0]} \n {jubileu_perdeu[1]} \n {jubileu_perdeu[2]} \n {jubileu_perdeu[3]} \n {jubileu_perdeu[4]} \n {jubileu_perdeu[5]} \n {jubileu_perdeu[6]} \n')
             print(f'\n A palavra correta era: {palavra}\n')
             break
 
